de4d80b8ce97a1a8cd154df1c29c136d0aa54a3a
15017470 <15017470@myrp.edu.sg>
2017-06-06 14:56:59 +0800

    Completed PS

--- app/src/main/java/com/example/a15017470/p06/FragmentFirst.java
+++ app/src/main/java/com/example/a15017470/p06/FragmentFirst.java
@@ -1,11 +1,21 @@
 package com.example.a15017470.p06;
 
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +23,66 @@ import android.view.ViewGroup;
  */
 public class FragmentFirst extends Fragment {
 
+    EditText et1;
+    Button btn1;
+    TextView tv1;
 
     public FragmentFirst() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
 
+        et1 = (EditText)view.findViewById(R.id.et1);
+        btn1 = (Button)view.findViewById(R.id.btn1);
+        tv1 = (TextView)view.findViewById(R.id.tv1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (getActivity(), Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission not
+                    //  granted yet
+                    return;
+                }
+
+                Uri uri = Uri.parse("content://sms");
+
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                ContentResolver cr = getActivity().getContentResolver();
+
+                String filter = "address LIKE ?";
+                String[] filterArgs = {"%" + et1.getText().toString() + "%"};
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                } tv1.setText(smsBody);
+            }
+        }); return view;
+    }
 }
--- app/src/main/java/com/example/a15017470/p06/FragmentSecond.java
+++ app/src/main/java/com/example/a15017470/p06/FragmentSecond.java
@@ -1,11 +1,21 @@
 package com.example.a15017470.p06;
 
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +23,9 @@ import android.view.ViewGroup;
  */
 public class FragmentSecond extends Fragment {
 
+    EditText et2;
+    Button btn2;
+    TextView tv2;
 
     public FragmentSecond() {
         // Required empty public constructor
@@ -23,7 +36,54 @@ public class FragmentSecond extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
 
+        et2 = (EditText)view.findViewById(R.id.et2);
+        btn2 = (Button)view.findViewById(R.id.btn2);
+        tv2 = (TextView)view.findViewById(R.id.tv2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (getActivity(), Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission not
+                    //  granted yet
+                    return;
+                }
+
+                Uri uri = Uri.parse("content://sms");
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                ContentResolver cr = getActivity().getContentResolver();
+
+                String filter = "body LIKE ?";
+                String[] filterArgs = {"%" + et2.getText().toString() + "%"};
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                } tv2.setText(smsBody);
+            }
+        }); return view;
+    }
 }
